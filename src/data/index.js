95999e5017e4f9f59ab5c5cb8ae59d0a39d23c14
LTN130925 <ltansang374@gmail.com>
2026-04-12 15:11:13 +0700

    update data nhap-mon-web.json

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,8 @@
 import congNghePhanMem from './cong-nghe-phan-mem.json';
 import coSoDuLieu from './co-so-du-lieu.json';
 import mangMayTinh from './mang-may-tinh.json';
-import kiemThuPhanMem from './kiem-thu-phan-mem.json'
+import kiemThuPhanMem from './kiem-thu-phan-mem.json';
+import nhapMonWeb from './nhap-mon-web.json';
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
@@ -12,7 +13,8 @@ const allQuizzes = [
   congNghePhanMem,
   coSoDuLieu,
   mangMayTinh,
-  kiemThuPhanMem
+  kiemThuPhanMem,
+  nhapMonWeb
 ];
 
 export default allQuizzes;
